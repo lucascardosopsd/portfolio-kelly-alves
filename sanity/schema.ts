092eeedf607e0ba0fb38f539cec8aeab0fc3064c
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-09 16:27:46 -0300

    schema: services

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from "sanity";
 import profile from "./schemas/profile";
 import hero from "./schemas/hero";
+import services from "./schemas/services";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile, hero],
+  types: [profile, hero, services],
 };
---
+++ sanity/schemas/services.ts
@@ -0,0 +1,51 @@
+export default {
+  name: "serviceCategories",
+  type: "document",
+  title: "Categorias de Serviços",
+  fields: [
+    {
+      name: "categoryName",
+      type: "string",
+      title: "Nome",
+    },
+    {
+      name: "categoryServices",
+      title: "Serviços",
+      type: "array",
+      of: [
+        {
+          name: "categoryService",
+          type: "object",
+          title: "Serviço",
+          fields: [
+            {
+              name: "order",
+              type: "number",
+              title: "Posição",
+            },
+            {
+              name: "title",
+              type: "string",
+              title: "Título",
+            },
+            {
+              name: "description",
+              type: "string",
+              title: "Descrição",
+            },
+            {
+              name: "mainImage",
+              type: "image",
+              title: "Imagem Principal",
+            },
+            {
+              name: "secondaryImage",
+              type: "image",
+              title: "Imagem Secundária",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
