3b40e9404b3cf71a4d8805061fb3d1da612d4dec
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-10 15:13:40 -0300

    layout: results section

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,12 +4,15 @@ import Hero from "@/sections/Hero";
 import Services from "@/sections/services";
 import { getServices } from "@/services/getServices";
 import Service from "@/sections/service";
-import Bio from "@/sections/bio";
+import Bio from "@/sections/Bio";
+import { getResults } from "@/services/getResults";
+import Results from "@/sections/Results";
 
 export default async function Home() {
   const heroData = await getHero();
   const profileData = await getProfile();
   const serviceCategoriesData = await getServices();
+  const ResultsData = await getResults();
 
   return (
     <>
@@ -26,6 +29,7 @@ export default async function Home() {
         ))
       )}
       <Bio profileData={profileData[0]} />
+      <Results resultsData={ResultsData} />
     </>
   );
 }
---
+++ src/sections/Results.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { watchInView } from "@/tools/watchInView";
+import { useRef } from "react";
+import FadeDiv from "@/components/FadeDiv";
+import Image from "next/image";
+import { ResultProps } from "@/types/result";
+import SectionHeading from "@/components/SectionHeading";
+
+export interface ResultsProps {
+  resultsData: ResultProps[];
+}
+
+const Results = ({ resultsData }: ResultsProps) => {
+  const ref = useRef(null);
+  watchInView({ ref, id: "results" });
+
+  return (
+    <FadeDiv>
+      <section
+        className="flex items-center justify-center max-width flex-col gap-4 py-4 text-beige-800 relative"
+        id="results"
+        ref={ref}
+      >
+        <SectionHeading
+          title="Resultdos"
+          subtitle="Essas são as provas do meu trabalho. Meus resultados são comprovados."
+        />
+        <div className="flex flex-col tablet:flex-row gap-4 w-full tablet:p-10 overflow-x-auto">
+          {resultsData.map((result, index) => (
+            <div
+              className="flex-1 flex flex-col h-full w-full tablet:min-w-[500px] border border-beige-600 rounded "
+              key={index}
+            >
+              <p className="text-beige-700 flex items-center justify-center font-normal h-10 text-2xl tablet:text-lg">
+                {result.title}
+              </p>
+              <div className="flex flex-col tablet:flex-row">
+                <div className="relative w-auto h-[300px] p-4 rounded">
+                  <Image
+                    alt="result"
+                    src={result.beforePic}
+                    height={0}
+                    width={0}
+                    sizes="100vh"
+                    className="h-full w-full object-cover rounded"
+                  />
+                </div>
+
+                <div className="relative w-auto h-[300px] p-4 rounded">
+                  <Image
+                    alt="result"
+                    src={result.afterPic}
+                    height={0}
+                    width={0}
+                    sizes="100vh"
+                    className="h-full w-full object-cover rounded"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </FadeDiv>
+  );
+};
+
+export default Results;
