8b498d5dd2a6fc9dc8d3bf4d5b80fbfa751797a2
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-10 17:58:49 -0300

    schema: ratings

---
+++ sanity/schemas/ratings.ts
@@ -0,0 +1,22 @@
+export default {
+  name: "ratings",
+  type: "document",
+  title: "Avaliações",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+      title: "Nome Da Cliente",
+    },
+    {
+      name: "testimony",
+      type: "string",
+      title: "Depoimento",
+    },
+    {
+      name: "clientPic",
+      type: "image",
+      title: "Foto da Cliente",
+    },
+  ],
+};
