626dd41226d1b9277a2eea7cc7c6ff9099a79b60
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-10 15:18:00 -0300

    layout: fade in results scroll

--- src/sections/Results.tsx
+++ src/sections/Results.tsx
@@ -25,7 +25,8 @@ const Results = ({ resultsData }: ResultsProps) => {
           title="Resultdos"
           subtitle="Essas são as provas do meu trabalho. Meus resultados são comprovados."
         />
-        <div className="flex flex-col tablet:flex-row gap-4 w-full tablet:p-10 overflow-x-auto">
+        <div className="relative flex flex-col tablet:flex-row gap-4 w-full tablet:p-10 overflow-x-auto">
+          <span className="bg-gradient-to-l from-beige-300 to-transparent h-full w-[200px] hidden tablet:block absolute right-0 top-0 bottom-0 my-0 z-10" />
           {resultsData.map((result, index) => (
             <div
               className="flex-1 flex flex-col h-full w-full tablet:min-w-[500px] border border-beige-600 rounded "
