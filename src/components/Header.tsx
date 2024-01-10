25b19e1f48c24ed8140ddd02845de53759a1c6da
Lucas Cardoso <lucascardosopsd@gmail.com>
2024-01-10 16:03:35 -0300

    feat: auto fixed header when scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,33 @@
+"use client";
+
 import NavLink from "./CustomLink";
 import MobileMenu from "./MobileMenu";
 import { navLinks } from "../constants";
 import { ProfileProps } from "@/types/profile";
+import { useEffect, useState } from "react";
 
 interface HeaderProps {
   profileData: ProfileProps;
 }
 
-const Header = async ({ profileData }: HeaderProps) => {
+const Header = ({ profileData }: HeaderProps) => {
+  const [fixedHeader, setFixedHeader] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      !fixedHeader && setFixedHeader(window.scrollY >= 200);
+
+      fixedHeader && setFixedHeader(window.scrollY <= 200);
+    });
+  }, []);
+
   return (
     <>
-      <div className="hidden tablet:flex w-full items-center">
+      <div
+        className={`hidden tablet:flex w-full items-center transition ${
+          fixedHeader ? "fixed top-0 bg-beige-400 z-30" : "static"
+        }`}
+      >
         <header className="flex items-center justify-between h-20 max-width w-full z-10">
           <nav className="flex items-center justify-center w-full gap-12 max-tablet:hidden">
             <ul className="flex items-center gap-8 text-lg">
